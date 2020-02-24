a487ddcdb3732dbb0cbb54f4709e9c33d12f54dd
iwarui722 <warui.r.s@gmail.com>
2020-02-24 16:52:41 +0300

    add code for development

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 //adding code for what we do
     $(document).ready(function(){
-        $("button").click(function(){
+        $("#btn1").click(function(){
             $(this).text($(this).text() == 'DESIGN' ? 'Our design practice offers a full range of services including branding strategy,interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual designs and workflows solicit user feedback, and work with you to make sure what gets built is what is needed.' : 'DESIGN');
+        });
+    });
+//adding code for    
+    $(document).ready(function(){
+        $("#btn2").click(function(){
+            $(this).text($(this).text() == 'DEVELOPMENT' ? 'All engineers are fluent in the latest enterprise, mobile and web development technologies. They collaborate with your team to write, test, and improve code on a daily basis, using proven practices such as test-driven development and pair programming' : 'DEVELOPMENT');
         });
     });
 
